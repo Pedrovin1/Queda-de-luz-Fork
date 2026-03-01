ef500bbc3d5c526659840d5c511360ed86ce94c3
NtanBraga <natanybraga@gmail.com>
2026-02-28 22:07:09 -0300

    adicionada lista nos bairros que estara faltando luz

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  
+</script>
 
 <style lang="scss">
 @import './assets/styles/App.scss';
@@ -9,7 +11,15 @@
     <div class="box-news"><h1 class="placard-h1">News</h1></div>
   </div>
   <div class="below-content">
-    <div class="box-power-outage"><h1>Power Outage</h1></div>
+    <div class="box-power-outage">
+      <ul class="lista-bairros-sem-luz">
+        <li class="lista-items-bairros-sem-luz"><strong>Bairro sem luz 1</strong></li>
+        <li class="lista-items-bairros-sem-luz"><strong>Bairro sem luz 2</strong></li>
+        <li class="lista-items-bairros-sem-luz"><strong>Bairro sem luz 3</strong></li>
+        <li class="lista-items-bairros-sem-luz"><strong>Bairro sem luz 4</strong></li>
+        <li class="lista-items-bairros-sem-luz"><strong>Bairro sem luz 5</strong></li>
+      </ul>
+    </div>
     <div class="box-map"><h1>Map</h1></div>
   </div>
 </template>
