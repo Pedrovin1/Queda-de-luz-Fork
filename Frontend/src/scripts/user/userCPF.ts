49d9433a69c9d50ecac843693b7005101b678023
NtanBraga <natanybraga@gmail.com>
2026-04-10 18:48:28 -0300

    aplicacao do banco de dados

--- Frontend/src/scripts/user/userCPF.ts
+++ Frontend/src/scripts/user/userCPF.ts
@@ -1,3 +1,5 @@
+const API_BANCO_DE_DADOS = 'http://localhost:5176/accounts'
+
 export interface UserCPF {
   nome: string
   cpf: string
@@ -13,9 +15,34 @@ export interface UserCPF {
 export const registrarContaCPF = async (userData: UserCPF) => {
   console.log(userData)
 
+  const payload = {
+    Username: userData.nome,
+    Unhashed_Password: userData.senha,
+    Email: userData.email,
+    District_Id: 1,
+    Person_Details: {
+      Birthday: userData.data_nascimento,
+    },
+    Business_Details: null,
+  }
+
   try {
-    console.log(`Usuario registrado no sistema: ${userData.nome}`)
+    const response = await fetch(API_BANCO_DE_DADOS, {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(payload),
+    })
+
+    if (!response.ok) {
+      const text = await response.text()
+      throw new Error(
+        `Erro ao enviar conta registrada para o banco de dados: ${response.status} -${text}`,
+      )
+    }
+    console.log(`Usuario registrado no sistema: ${payload.Username}`)
+    return await response.json()
   } catch (e) {
     console.error('Erro ao tentar criar conta: ', e)
+    throw e
   }
 }
